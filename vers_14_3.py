359dbb91baab9756c4b6ba9ffea6b3f68cfc41a2
Galactica91 <osheva91@yandex.com>
2024-12-16 16:41:51 +0300

    Update vers_14_3.py

--- vers_14_3.py
+++ vers_14_3.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7775779452:AAEATs0Fe0bP-tyd7DfnOmL3nOPUGEWIEI4"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
